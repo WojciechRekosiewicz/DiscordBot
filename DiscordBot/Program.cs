c412dd421b95fc55c7cc5880e9b1c40acff33b7b
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-09-10 18:14:26 +0200

    change c# version for from 7.0 to 7.3

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,10 +1,11 @@
 ﻿using DiscordBot.Storage.Impl;
 using System;
 using Discord.WebSocket;
+using System.Threading.Tasks;
 
 namespace DiscordBot
 {
-    class Program
+     class Program
     {
         static void Main()
         {
@@ -15,11 +16,13 @@ namespace DiscordBot
             var config = new DiscordSocketConfig
             {
                 AlwaysDownloadUsers = true
+
             };
             var client = new DiscordSocketClient(config);
             Console.WriteLine("HW!");
 
-         
+
+
         }
     }
 

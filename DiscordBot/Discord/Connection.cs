c5975194a03c1afe6e2245029225a1c8f302c931
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-09-12 16:04:06 +0200

    added two logger class and add things to connection class

--- DiscordBot/Discord/Connection.cs
+++ DiscordBot/Discord/Connection.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using DiscordBot.Discord.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DiscordBot.Discord
 {
-    class Connection
+    public class Connection
     {
+        private DiscordSocketClient _client;
+
+
+        internal async Task ConnectAsync(DiscordBotConfig config)
+        {
+            _client = new DiscordSocketClient(config.SocketConfig);
+
+            _client.Log += Log;
+                       
+             
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

1a9a21bee1642e60d08339e018352afda6f23a03
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-09-12 21:57:58 +0200

    added JsonStorage with IDataStorage impl

--- DiscordBot/Storage/Impl/JsonStorage.cs
+++ DiscordBot/Storage/Impl/JsonStorage.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using Newtonsoft.Json;
+using static System.IO.Directory;
 
 namespace DiscordBot.Storage.Impl
 {
-    class JsonStorage
+    class JsonStorage : IDataStorage
     {
+        public T RestoreObject<T>(string key)
+        {
+            var json = File.ReadAllText($"{key}.json");
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public void StoreObject(object obj, string key)
+        {
+            var file = $"{key}.json";
+            CreateDirectory(file);
+            var json = JsonConvert.SerializeObject(obj);
+            File.WriteAllText(file, json);
+
+        }
     }
 }
